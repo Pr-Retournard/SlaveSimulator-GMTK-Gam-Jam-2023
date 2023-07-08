9e1ae868966d8f624f63c2e33707bc6319bb0b90
v1rg1le <v1rg1le@hotmail.fr>
2023-07-08 16:31:04 +0200

    8 directions for player

--- src/Character/player.gd
+++ src/Character/player.gd
@@ -5,12 +5,19 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 var dir : Vector2
 
+@onready var animation_tree : AnimationTree = $AnimationTree
+
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	velocity = dir * SPEED
 	velocity.normalized()
 	move_and_slide()
+	
+	animation_tree.set("parameters/Move/blend_position", dir)
 	
 func _unhandled_input(_event : InputEvent):
 	dir.x = Input.get_axis("ui_left","ui_right")
