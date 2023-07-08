d0edfcdb57dd6b7dc519061e89fb4524bc39ebcb
Pr-Retournard <emmanuel.retournard@gmail.com>
2023-07-08 10:51:36 +0200

    clean code player + changement de map

--- src/Character/player.gd
+++ src/Character/player.gd
@@ -2,25 +2,16 @@ extends CharacterBody2D
 #Source tilemap https://kenney.nl/assets/isometric-blocks
 #https://www.youtube.com/watch?v=SiuVvskVs-0 - How to make isometric pixelart worldmap in Godot - Professor Bubbles Devlog #6
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@export var SPEED = 300.0
+var dir : Vector2
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionx = Input.get_axis("ui_left", "ui_right")
-	if directionx:
-		velocity.x = directionx * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	var directiony = Input.get_axis("ui_up", "ui_down")
-	if directiony:
-		velocity.y = directiony * SPEED
-	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
+	velocity = dir * SPEED
 	velocity.normalized()
 	move_and_slide()
+	
+func _unhandled_input(_event : InputEvent):
+	dir.x = Input.get_axis("ui_left","ui_right")
+	dir.y = Input.get_axis("ui_up","ui_down")
