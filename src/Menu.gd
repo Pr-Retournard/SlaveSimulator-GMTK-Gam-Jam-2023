e96306e1c5f40168c4041fe18159956191f648ce
Pr-Retournard <emmanuel.retournard@gmail.com>
2023-07-12 23:28:27 +0200

    Menu assets

--- src/Menu.gd
+++ src/Menu.gd
@@ -13,3 +13,11 @@ func _process(delta):
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://src/world.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://src/credits.tscn")
